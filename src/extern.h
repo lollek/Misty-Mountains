0e0d3f715b7cae92cb0024cbe3cbd4de00c162ee
Olle K <olle.kvarnstroem@gmail.com>
2014-06-09 20:05:27 +0200

    cleaned up extern.h

--- src/extern.h
+++ src/extern.h
@@ -1,7 +1,7 @@
 /*
  * Defines for things used in mach_dep.c
  *
- * @(#)extern.h	4.35 (Berkeley) 02/05/99
+ * @(#)extern.h 4.35 (Berkeley) 02/05/99
  *
  * Rogue: Exploring the Dungeons of Doom
  * Copyright (C) 1980-1983, 1985, 1999 Michael Toy, Ken Arnold and Glenn Wichman
@@ -9,88 +9,6 @@
  *
  * See the file LICENSE.TXT for full copyright and licensing information.
  */
-
-
-#ifdef HAVE_CONFIG_H
-#ifdef PDCURSES
-#undef HAVE_UNISTD_H
-#undef HAVE_LIMITS_H
-#undef HAVE_MEMORY_H
-#undef HAVE_STRING_H
-#endif
-#include "config.h"
-#elif defined(__DJGPP__)
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_PROCESS_H 1
-#define HAVE_PWD_H 1
-#define HAVE_TERMIOS_H 1
-#define HAVE_SETGID 1
-#define HAVE_GETGID 1
-#define HAVE_SETUID 1
-#define HAVE_GETUID 1
-#define HAVE_GETPASS 1
-#define HAVE_SPAWNL 1
-#define HAVE_ALARM 1
-#define HAVE_ERASECHAR 1
-#define HAVE_KILLCHAR 1
-#elif defined(_WIN32)
-#define HAVE_CURSES_H
-#define HAVE_TERM_H
-#define HAVE__SPAWNL
-#define HAVE_SYS_TYPES_H
-#define HAVE_PROCESS_H
-#define HAVE_ERASECHAR 1
-#define HAVE_KILLCHAR 1
-#elif defined(__CYGWIN__)
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_PWD_H 1
-#define HAVE_PWD_H 1
-#define HAVE_SYS_UTSNAME_H 1
-#define HAVE_ARPA_INET_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_TERMIOS_H 1
-#define HAVE_NCURSES_TERM_H 1
-#define HAVE_SETGID 1
-#define HAVE_GETGID 1
-#define HAVE_SETUID 1
-#define HAVE_GETUID 1
-#define HAVE_GETPASS 1
-#define HAVE_GETPWUID 1
-#define HAVE_WORKING_FORK 1
-#define HAVE_ALARM 1
-#define HAVE_SPAWNL 1
-#define HAVE__SPAWNL 1
-#define HAVE_ERASECHAR 1
-#define HAVE_KILLCHAR 1
-#else /* POSIX */
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_PWD_H 1
-#define HAVE_PWD_H 1
-#define HAVE_SYS_UTSNAME_H 1
-#define HAVE_ARPA_INET_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_TERMIOS_H 1
-#define HAVE_TERM_H 1
-#define HAVE_SETGID 1
-#define HAVE_GETGID 1
-#define HAVE_SETUID 1
-#define HAVE_GETUID 1
-#define HAVE_SETREUID 1
-#define HAVE_SETREGID 1
-#define HAVE_GETPASS 1
-#define HAVE_GETPWUID 1
-#define HAVE_WORKING_FORK 1
-#define HAVE_ERASECHAR 1
-#define HAVE_KILLCHAR 1
-#ifndef _AIX
-#define HAVE_GETLOADAVG 1
-#endif
-#define HAVE_ALARM 1
-#endif
-
-#ifdef __DJGPP__
-#undef HAVE_GETPWUID /* DJGPP's limited version doesn't even work as documented */
-#endif
 
 /*
  * Don't change the constants, since they are used for sizes in many
@@ -101,71 +19,66 @@
 
 #undef SIGTSTP
 
-#define MAXSTR		1024	/* maximum length of strings */
-#define MAXLINES	32	/* maximum number of screen lines used */
-#define MAXCOLS		80	/* maximum number of screen columns used */
+#define MAXSTR 1024 /* maximum length of strings */
+#define MAXLINES 32 /* maximum number of screen lines used */
+#define MAXCOLS  80 /* maximum number of screen columns used */
 
-#define RN		(((seed = seed*11109+13849) >> 16) & 0xffff)
-#ifdef CTRL
+#define RN  (((seed = seed*11109+13849) >> 16) & 0xffff)
+
 #undef CTRL
-#endif
-#define CTRL(c)		(c & 037)
+#define CTRL(c) (c & 037)
 
 /*
  * Now all the global variables
  */
 
-extern bool	got_ltc, in_shell;
-extern int	wizard;
-extern char	fruit[], prbuf[], whoami[];
+extern bool got_ltc, in_shell;
+extern int wizard;
+extern char fruit[], prbuf[], whoami[];
 extern int orig_dsusp;
-extern FILE	*scoreboard;
+extern FILE *scoreboard;
 
 /*
  * Function types
  */
 
-void    auto_save(int);
-void	come_down();
-void	doctor();
-void	end_line();
-void    endit(int sig);
-void	fatal();
-void	land();
-void    leave(int);
-void	my_exit();
-void	nohaste();
-void	playit();
-void	print_disc(char);
-void    quit(int);
-void	rollwand();
-void	runners();
-void	set_order();
-void	sight();
-void	stomach();
-void	swander();
-void	tstp(int ignored);
-void	unconfuse();
-void	unsee();
-void	visuals();
+void auto_save(int);
+void come_down();
+void doctor();
+void end_line();
+void endit(int sig);
+void fatal();
+void land();
+void leave(int);
+void my_exit();
+void nohaste();
+void playit();
+void print_disc(char);
+void quit(int);
+void rollwand();
+void runners();
+void set_order();
+void sight();
+void stomach();
+void swander();
+void tstp(int ignored);
+void unconfuse();
+void unsee();
+void visuals();
 
-char	add_line(char *fmt, char *arg);
+char add_line(char *fmt, char *arg);
 
-char	*killname(char monst, bool doart);
-char	*nothing(char type);
-char	*type_name(int type);
+char *killname(char monst, bool doart);
+char *nothing(char type);
+char *type_name(int type);
 
-#ifdef CHECKTIME
-int	checkout();
-#endif
-
-char	*md_crypt(char *key, char *salt);
-char	*md_gethomedir();
-char	*md_getusername();
-char	*md_getrealname(int uid);
-void	md_normaluser();
-int	md_readchar();
-int	md_shellescape();
-int	md_hasclreol();
+char *md_crypt(char *key, char *salt);
+char *md_gethomedir();
+char *md_getusername();
+char *md_getrealname(int uid);
+void md_normaluser();
+int md_readchar();
+int md_shellescape();
+int md_hasclreol();
 int md_issymlink(char *sp);
 
