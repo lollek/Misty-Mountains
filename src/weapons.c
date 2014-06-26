0d84d873d96631ba7fe5f174ce5d7840047a7ab0
Olle K <olle.kvarnstroem@gmail.com>
2014-06-27 00:47:39 +0200

    cleaned weapons.c

--- src/weapons.c
+++ src/weapons.c
@@ -35,249 +35,234 @@ static struct init_weaps {
     { "2x3",	"1x6",	NO_WEAPON,	ISMISL,		},	/* Spear */
 };
 
-/*
- * missile:
- *	Fire a missile in a given direction
- */
-
+/** missile:
+ * Fire a missile in a given direction */
 bool
 missile(int ydelta, int xdelta)
 {
-    THING *obj;
+  THING *obj = get_item("throw", WEAPON);
 
-    /* Get which thing we are hurling */
-    if ((obj = get_item("throw", WEAPON)) == NULL)
-	return true;
-    if (!dropcheck(obj) || is_current(obj))
-	return true;
-    obj = leave_pack(obj, true, false);
-    do_motion(obj, ydelta, xdelta);
+  if (obj == NULL)
+    return false;
 
-    /* AHA! Here it has hit something.  If it is a wall or a door,
-     * or if it misses (combat) the monster, put it on the floor */
-    if (moat(obj->o_pos.y, obj->o_pos.x) == NULL ||
-	!hit_monster(obj->o_pos.y, obj->o_pos.x, obj))
-	    fall(obj, true);
+  if (obj->o_flags & ISCURSED)
+  {
+    msg("you can't.  It appears to be cursed");
     return true;
+  }
+
+  if (obj == cur_weapon)
+    cur_weapon = NULL;
+
+  obj = leave_pack(obj, true, false);
+  do_motion(obj, ydelta, xdelta);
+
+  /* AHA! Here it has hit something.  If it is a wall or a door,
+   * or if it misses (combat) the monster, put it on the floor */
+  if (moat(obj->o_pos.y, obj->o_pos.x) == NULL ||
+      !hit_monster(obj->o_pos.y, obj->o_pos.x, obj))
+    fall(obj, true);
+
+  return true;
 }
 
-/*
- * do_motion:
- *	Do the actual motion on the screen done by an object traveling
- *	across the room
- */
-
+/** do_motion:
+ * Do the actual motion on the screen done by an object traveling
+ * across the room */
 void
 do_motion(THING *obj, int ydelta, int xdelta)
 {
-    int ch;
+  int ch;
 
-    /*
-     * Come fly with us ...
-     */
-    obj->o_pos = hero;
-    for (;;)
+  /* Come fly with us ... */
+  obj->o_pos = hero;
+  for (;;)
+  {
+    /* Erase the old one */
+    if (!same_coords(obj->o_pos, hero) &&
+        cansee(obj->o_pos.y, obj->o_pos.x) && !terse)
     {
-	/*
-	 * Erase the old one
-	 */
-	if (!same_coords(obj->o_pos, hero) &&
-            cansee(obj->o_pos.y, obj->o_pos.x) && !terse)
-	{
-	    ch = chat(obj->o_pos.y, obj->o_pos.x);
-	    if (ch == FLOOR && !show_floor())
-		ch = SHADOW;
-	    mvaddcch(obj->o_pos.y, obj->o_pos.x, ch);
-	}
-	/*
-	 * Get the new position
-	 */
-	obj->o_pos.y += ydelta;
-	obj->o_pos.x += xdelta;
-	if (step_ok(ch = winat(obj->o_pos.y, obj->o_pos.x)) && ch != DOOR)
-	{
-	    /*
-	     * It hasn't hit anything yet, so display it
-	     * If it alright.
-	     */
-	    if (cansee(obj->o_pos.y, obj->o_pos.x) && !terse)
-	    {
-		mvaddcch(obj->o_pos.y, obj->o_pos.x, obj->o_type);
-		refresh();
-	    }
-	    continue;
-	}
-	break;
+      ch = chat(obj->o_pos.y, obj->o_pos.x);
+      if (ch == FLOOR && !show_floor())
+        ch = SHADOW;
+      mvaddcch(obj->o_pos.y, obj->o_pos.x, ch);
     }
+
+    /* Get the new position */
+    obj->o_pos.y += ydelta;
+    obj->o_pos.x += xdelta;
+    if (step_ok(ch = winat(obj->o_pos.y, obj->o_pos.x)) && ch != DOOR)
+    {
+      /* It hasn't hit anything yet, so display it if it alright. */
+      if (cansee(obj->o_pos.y, obj->o_pos.x) && !terse)
+      {
+        mvaddcch(obj->o_pos.y, obj->o_pos.x, obj->o_type);
+        refresh();
+      }
+      continue;
+    }
+    break;
+  }
 }
 
-/*
- * fall:
- *	Drop an item someplace around here.
- */
-
+/** fall:
+ * Drop an item someplace around here. */
 void
 fall(THING *obj, bool pr)
 {
-    PLACE *pp;
-    static coord fpos;
+  PLACE *pp;
+  static coord fpos;
 
-    if (fallpos(&obj->o_pos, &fpos))
+  if (fallpos(&obj->o_pos, &fpos))
+  {
+    pp = INDEX(fpos.y, fpos.x);
+    pp->p_ch = (char) obj->o_type;
+    obj->o_pos = fpos;
+    if (cansee(fpos.y, fpos.x))
     {
-	pp = INDEX(fpos.y, fpos.x);
-	pp->p_ch = (char) obj->o_type;
-	obj->o_pos = fpos;
-	if (cansee(fpos.y, fpos.x))
-	{
-	    if (pp->p_monst != NULL)
-		pp->p_monst->t_oldch = (char) obj->o_type;
-	    else
-		mvaddcch(fpos.y, fpos.x, obj->o_type);
-	}
-	attach(lvl_obj, obj);
-	return;
+      if (pp->p_monst != NULL)
+        pp->p_monst->t_oldch = (char) obj->o_type;
+      else
+        mvaddcch(fpos.y, fpos.x, obj->o_type);
     }
-    if (pr)
+    attach(lvl_obj, obj);
+    return;
+  }
+
+  if (pr)
+  {
+    if (has_hit)
     {
-	if (has_hit)
-	{
-	    endmsg();
-	    has_hit = false;
-	}
-	msg("the %s vanishes as it hits the ground",
-	    weap_info[obj->o_which].oi_name);
+      endmsg();
+      has_hit = false;
     }
-    discard(obj);
+    msg("the %s vanishes as it hits the ground",
+        weap_info[obj->o_which].oi_name);
+  }
+  discard(obj);
 }
 
-/*
- * init_weapon:
- *	Set up the initial goodies for a weapon
- */
+/** init_weapon:
+ * Set up the initial goodies for a weapon */
 
 void
 init_weapon(THING *weap, int which)
 {
-    struct init_weaps *iwp;
+  struct init_weaps *iwp;
 
-    weap->o_type = WEAPON;
-    weap->o_which = which;
-    iwp = &init_dam[which];
-    strncpy(weap->o_damage, iwp->iw_dam, sizeof(weap->o_damage));
-    strncpy(weap->o_hurldmg,iwp->iw_hrl, sizeof(weap->o_hurldmg));
-    weap->o_launch = iwp->iw_launch;
-    weap->o_flags = iwp->iw_flags;
-    weap->o_hplus = 0;
-    weap->o_dplus = 0;
-    if (which == DAGGER)
-    {
-	weap->o_count = rnd(4) + 2;
-	weap->o_group = group++;
-    }
-    else if (weap->o_flags & ISMANY)
-    {
-	weap->o_count = rnd(8) + 8;
-	weap->o_group = group++;
-    }
-    else
-    {
-	weap->o_count = 1;
-	weap->o_group = 0;
-    }
+  weap->o_type = WEAPON;
+  weap->o_which = which;
+  iwp = &init_dam[which];
+  strncpy(weap->o_damage, iwp->iw_dam, sizeof(weap->o_damage));
+  strncpy(weap->o_hurldmg,iwp->iw_hrl, sizeof(weap->o_hurldmg));
+  weap->o_launch = iwp->iw_launch;
+  weap->o_flags = iwp->iw_flags;
+  weap->o_hplus = 0;
+  weap->o_dplus = 0;
+
+  if (which == DAGGER)
+  {
+    weap->o_count = rnd(4) + 2;
+    weap->o_group = group++;
+  }
+  else if (weap->o_flags & ISMANY)
+  {
+    weap->o_count = rnd(8) + 8;
+    weap->o_group = group++;
+  }
+  else
+  {
+    weap->o_count = 1;
+    weap->o_group = 0;
+  }
 }
 
-/*
- * hit_monster:
- *	Does the missile hit the monster?
- */
+/** hit_monster:
+ * Does the missile hit the monster? */
 int
 hit_monster(int y, int x, THING *obj)
 {
-    static coord mp;
+  static coord mp;
 
-    mp.y = y;
-    mp.x = x;
-    return fight(&mp, obj, true);
+  mp.y = y;
+  mp.x = x;
+  return fight(&mp, obj, true);
 }
 
-/*
- * num:
- *	Figure out the plus number for armor/weapons
- */
+/** num:
+ * Figure out the plus number for armor/weapons */
 char *
 num(int n1, int n2, char type)
 {
-    static char numbuf[10];
+  static char numbuf[10];
 
-    sprintf(numbuf, n1 < 0 ? "%d" : "+%d", n1);
-    if (type == WEAPON)
-	sprintf(&numbuf[strlen(numbuf)], n2 < 0 ? ",%d" : ",+%d", n2);
-    return numbuf;
+  sprintf(numbuf, n1 < 0 ? "%d" : "+%d", n1);
+  if (type == WEAPON)
+    sprintf(&numbuf[strlen(numbuf)], n2 < 0 ? ",%d" : ",+%d", n2);
+  return numbuf;
 }
 
-/*
- * wield:
- *	Pull out a certain weapon
- */
-
+/** wield:
+ * Pull out a certain weapon */
 bool
 wield()
 {
-    THING *obj;
-    THING *oweapon = cur_weapon;
-    char *sp;
+  THING *obj = get_item("wield", WEAPON);
+  THING *oweapon = cur_weapon;
+  char *sp;
 
-    if (!dropcheck(cur_weapon))
-    {
-	cur_weapon = oweapon;
-	return true;
-    }
-    cur_weapon = oweapon;
-    if ((obj = get_item("wield", WEAPON)) == NULL)
-	return false;
+  if (obj == NULL)
+    return false;
 
-    if (obj->o_type == ARMOR)
-    {
-	msg("you can't wield armor");
-	return false;
-    }
-    if (is_current(obj))
-	return false;
+  /* Check if already in use */
+  if (is_current(obj))
+    return wield();
 
-    sp = inv_name(obj, true);
-    cur_weapon = obj;
-    if (!terse)
-	addmsg("you are now ");
-    msg("wielding %s (%c)", sp, obj->o_packch);
+  if (obj->o_type == ARMOR)
+  {
+    msg("you can't wield armor");
+    return wield();
+  }
+
+  if (cur_weapon != NULL && cur_weapon->o_flags & ISCURSED)
+  {
+    msg("you can't. Your weapon appears to be cursed");
     return true;
+  }
+
+  sp = inv_name(obj, true);
+  cur_weapon = obj;
+
+  if (!terse)
+    addmsg("you are now ");
+  msg("wielding %s (%c)", sp, obj->o_packch);
+  return true;
 }
 
-/*
- * fallpos:
- *	Pick a random position around the give (y, x) coordinates
- */
+/** fallpos:
+ * Pick a random position around the give (y, x) coordinates */
 bool
 fallpos(coord *pos, coord *newpos)
 {
-    int y, x, cnt, ch;
+  int y, x, cnt, ch;
 
-    cnt = 0;
-    for (y = pos->y - 1; y <= pos->y + 1; y++)
-	for (x = pos->x - 1; x <= pos->x + 1; x++)
-	{
-	    /*
-	     * check to make certain the spot is empty, if it is,
-	     * put the object there, set it in the level list
-	     * and re-draw the room if he can see it
-	     */
-	    if (y == hero.y && x == hero.x)
-		continue;
-	    if (((ch = chat(y, x)) == FLOOR || ch == PASSAGE)
-					&& rnd(++cnt) == 0)
-	    {
-		newpos->y = y;
-		newpos->x = x;
-	    }
-	}
-    return (bool)(cnt != 0);
+  cnt = 0;
+  for (y = pos->y - 1; y <= pos->y + 1; y++)
+    for (x = pos->x - 1; x <= pos->x + 1; x++)
+    {
+      /*
+       * check to make certain the spot is empty, if it is,
+       * put the object there, set it in the level list
+       * and re-draw the room if he can see it
+       */
+      if (y == hero.y && x == hero.x)
+        continue;
+      if (((ch = chat(y, x)) == FLOOR || ch == PASSAGE)
+          && rnd(++cnt) == 0)
+      {
+        newpos->y = y;
+        newpos->x = x;
+      }
+    }
+  return cnt != 0;
 }
