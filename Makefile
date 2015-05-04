ee39ae0b5879f819ff34c90a5c32001eec6c767d
Olle Kvarnström <olle+git@iix.se>
2015-05-04 13:36:23 +0200

    Dont spawn wandering monsters

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@
 
 
 PROGRAM  = rogue14
-VERSION  = 1.2.4
+VERSION  = 1.2.5
 PREFIX   = /usr/local
 SCOREDIR = $(PREFIX)/share/$(PROGRAM)/
 
--- src/init.c
+++ src/init.c
@@ -418,7 +418,6 @@ init_new_game(void)
   /* Start up daemons and fuses */
   daemon_start(daemon_runners_move, 0, AFTER);
   daemon_start(daemon_doctor, 0, AFTER);
-  daemon_start_fuse(daemon_start_wanderer, 0, WANDERTIME, AFTER);
   daemon_start(daemon_digest_food, 0, AFTER);
 
   return true;
