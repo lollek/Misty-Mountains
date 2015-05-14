e10f967fdf0e0849c1895f4515a74900111a0504
Olle Kvarnström <olle+git@iix.se>
2015-05-14 14:05:19 +0200

    move stuff to things.h

--- src/rogue.h
+++ src/rogue.h
@@ -16,6 +16,7 @@
 #include <ncurses.h>
 
 #include "coord.h"
+#include "things.h"
 
 /* Version */
 #define GAME_VERSION "Rogue14 " VERSION " - Based on Rogue5.4.4"
@@ -152,12 +153,6 @@ extern enum rogue_game_t
 #define F_PNUM		0x0f		/* passage number mask */
 #define F_TMASK		0x07		/* trap number mask */
 
-
-
-/*
- * Now we define the structures and types
- */
-
 /*
  * Help list
  */
@@ -165,17 +160,6 @@ struct h_list {
     char h_ch;
     char *h_desc;
     bool h_print;
-};
-
-/*
- * Stuff about objects
- */
-struct obj_info {
-    char *oi_name;
-    int oi_prob;
-    int oi_worth;
-    char *oi_guess;
-    bool oi_know;
 };
 
 /*
@@ -190,90 +174,6 @@ struct room {
     int r_nexits;			/* Number of exits */
     coord r_exit[12];			/* Where the exits are */
 };
-
-/*
- * Structure describing a fighting being
- */
-struct stats {
-    unsigned s_str;			/* Strength */
-    int s_exp;				/* Experience */
-    int s_lvl;				/* level of mastery */
-    int s_arm;				/* Armor class */
-    int s_hpt;			/* Hit points */
-    char s_dmg[13];			/* String describing damage done */
-    int  s_maxhp;			/* Max hit points */
-};
-
-/*
- * Structure for monsters and player
- */
-union thing {
-    struct {
-	union thing *_l_next, *_l_prev;	/* Next pointer in link */
-	coord _t_pos;			/* Position */
-	bool _t_turn;			/* If slowed, is it a turn to move */
-	char _t_type;			/* What it is */
-	char _t_disguise;		/* What mimic looks like */
-	char _t_oldch;			/* Character that was where it was */
-	coord *_t_dest;			/* Where it is running to */
-	unsigned _t_flags;		/* State word */
-	struct stats _t_stats;		/* Physical description */
-	struct room *_t_room;		/* Current room for thing */
-	union thing *_t_pack;		/* What the thing is carrying */
-        int _t_reserved;
-    } _t;
-    struct {
-	union thing *_l_next, *_l_prev;	/* Next pointer in link */
-	int _o_type;			/* What kind of object it is */
-	coord _o_pos;			/* Where it lives on the screen */
-	char *_o_text;			/* What it says if you read it */
-	int  _o_launch;			/* What you need to launch it */
-	char _o_packch;			/* What character it is in the pack */
-	char _o_damage[8];		/* Damage if used like sword */
-	char _o_hurldmg[8];		/* Damage if thrown */
-	int _o_count;			/* count for plural objects */
-	int _o_which;			/* Which object of a type it is */
-	int _o_hplus;			/* Plusses to hit */
-	int _o_dplus;			/* Plusses to damage */
-	int _o_arm;			/* Armor protection */
-	int _o_flags;			/* information about objects */
-	int _o_group;			/* group number for this object */
-	char *_o_label;			/* Label for object */
-    } _o;
-};
-
-typedef union thing THING;
-
-#define l_next		_t._l_next
-#define l_prev		_t._l_prev
-#define t_pos		_t._t_pos
-#define t_turn		_t._t_turn
-#define t_type		_t._t_type
-#define t_disguise	_t._t_disguise
-#define t_oldch		_t._t_oldch
-#define t_dest		_t._t_dest
-#define t_flags		_t._t_flags
-#define t_stats		_t._t_stats
-#define t_pack		_t._t_pack
-#define t_room		_t._t_room
-#define t_reserved      _t._t_reserved
-#define o_type		_o._o_type
-#define o_pos		_o._o_pos
-#define o_text		_o._o_text
-#define o_launch	_o._o_launch
-#define o_packch	_o._o_packch
-#define o_damage	_o._o_damage
-#define o_hurldmg	_o._o_hurldmg
-#define o_count		_o._o_count
-#define o_which		_o._o_which
-#define o_hplus		_o._o_hplus
-#define o_dplus		_o._o_dplus
-#define o_arm		_o._o_arm
-#define o_charges	o_arm
-#define o_goldval	o_arm
-#define o_flags		_o._o_flags
-#define o_group		_o._o_group
-#define o_label		_o._o_label
 
 /*
  * describe a place on the level map
