db998f28bf74cd6a00ba75026f0f731bc23eb4d6
Olle Kvarnström <olle+git@iix.se>
2015-05-22 22:39:09 +0200

    cleanup

--- src/rip.c
+++ src/rip.c
@@ -33,7 +33,7 @@
 
 #include "rip.h"
 
-static char* rip[] = {
+static char const* rip[] = {
 "                       __________\n",
 "                      /          \\\n",
 "                     /    REST    \\\n",
@@ -131,12 +131,61 @@ score_insert(SCORE* top_ten, SCORE* endp, int amount, int flags, char monst)
   return true;
 }
 
+static void
+score_print(SCORE* top_ten, SCORE* endp, int flags, int prflags)
+{
+  char* reason[] = { "killed", "quit", "A total winner", "killed with Amulet" };
+  char buf[2*MAXSTR];
+  if (flags != -1)
+    putchar('\n');
+
+  printf("Top %s %s:\n", NUMNAME, "Scores");
+  printf("   Score Name\n");
+  for (SCORE* ptr = top_ten; ptr < endp; ptr++)
+  {
+    if (!ptr->sc_score)
+      break;
+
+    printf("%2d %5d %s: %s on level %d"
+        ,ptr - top_ten + 1       /* Position */
+        ,ptr->sc_score           /* Score */
+        ,ptr->sc_name            /* Name */
+        ,reason[ptr->sc_flags]   /* Cause of death */
+        ,ptr->sc_level);         /* Death level */
+
+    if (ptr->sc_flags == 0 || ptr->sc_flags == 3)
+      printf(" by %s", killname(buf, (char) ptr->sc_monster, true));
+
+    if (prflags == 2)
+    {
+      fflush(stdout);
+      (void) fgets(buf,10,stdin);
+      if (buf[0] == 'd')
+      {
+        SCORE* sc2;
+        for (sc2 = ptr; sc2 < endp - 1; sc2++)
+          *sc2 = *(sc2 + 1);
+        sc2 = endp - 1;
+        sc2->sc_score = 0;
+        for (int i = 0; i < MAXSTR; i++)
+          sc2->sc_name[i] = (char) rnd(255);
+        sc2->sc_flags = rand_r(&seed);
+        sc2->sc_level = rand_r(&seed);
+        sc2->sc_monster = rand_r(&seed);
+        ptr--;
+      }
+    }
+    else
+      printf(".");
+    putchar('\n');
+  }
+}
+
 void
 score(int amount, int flags, char monst)
 {
   int prflags = 0;
   char buf[2*MAXSTR];
-  char *reason[] = { "killed", "quit", "A total winner", "killed with Amulet" };
 
   if (flags >= 0 || wizard)
   {
@@ -178,123 +227,85 @@ score(int amount, int flags, char monst)
   if (!wizard)
     in_highscore = score_insert(top_ten, endp, amount, flags, monst);
 
-  /* Print the list */
-  if (flags != -1)
-    putchar('\n');
+  /* Print the highscore */
+  score_print(top_ten, endp, flags, prflags);
 
-  printf("Top %s %s:\n", NUMNAME, "Scores");
-  printf("   Score Name\n");
-  for (SCORE* ptr = top_ten; ptr < endp; ptr++)
-  {
-    if (!ptr->sc_score)
-      break;
-
-    printf("%2d %5d %s: %s on level %d"
-           ,ptr - top_ten + 1       /* Position */
-           ,ptr->sc_score           /* Score */
-           ,ptr->sc_name            /* Name */
-           ,reason[ptr->sc_flags]   /* Cause of death */
-           ,ptr->sc_level);         /* Death level */
-
-    if (ptr->sc_flags == 0 || ptr->sc_flags == 3)
-      printf(" by %s", killname(buf, (char) ptr->sc_monster, true));
-
-    if (prflags == 2)
-    {
-      fflush(stdout);
-      (void) fgets(buf,10,stdin);
-      if (buf[0] == 'd')
-      {
-        SCORE* sc2;
-        for (sc2 = ptr; sc2 < endp - 1; sc2++)
-          *sc2 = *(sc2 + 1);
-        sc2 = endp - 1;
-        sc2->sc_score = 0;
-        for (int i = 0; i < MAXSTR; i++)
-          sc2->sc_name[i] = (char) rnd(255);
-        sc2->sc_flags = rand_r(&seed);
-        sc2->sc_level = rand_r(&seed);
-        sc2->sc_monster = rand_r(&seed);
-        ptr--;
-      }
-    }
-    else
-      printf(".");
-    putchar('\n');
-  }
-
-    /** Update the list file */
-    if (in_highscore)
-      score_write(top_ten);
+  /** Update the list file */
+  if (in_highscore)
+    score_write(top_ten);
 }
 
 void
 death(char monst)
 {
-    char **dp, *killer;
-    struct tm *lt;
-    static time_t date;
-    char buf[2*MAXSTR];
-    struct tm *localtime();
+  char buf[2*MAXSTR];
 
-    signal(SIGINT, SIG_IGN);
-    purse -= purse / 10;
-    signal(SIGINT, command_signal_leave);
-    clear();
-    killer = killname(buf, monst, false);
+  signal(SIGINT, SIG_IGN);
 
-    time(&date);
-    lt = localtime(&date);
-    move(8, 0);
-    dp = rip;
-    while (*dp)
-        addstr(*dp++);
-    mvaddstr(17, center(killer), killer);
-    if (monst == 's' || monst == 'h')
-        mvaddcch(16, 32, ' ');
-    else
-        mvaddstr(16, 33, vowelstr(killer));
-    mvaddstr(14, center(whoami), whoami);
-    sprintf(buf, "%d Au", purse);
-    move(15, center(buf));
-    addstr(buf);
-    sprintf(buf, "%4d", 1900+lt->tm_year);
-    mvaddstr(18, 26, buf);
+  purse -= purse / 10;
 
-    mvprintw(LINES -1, 0, "[Press return to continue]");
-    refresh();
-    wait_for(KEY_ENTER);
-    pack_evaluate();
-    score(purse, pack_contains_amulet() ? 3 : 0, monst);
-    exit(0);
+  signal(SIGINT, command_signal_leave);
+
+  clear();
+  char const* killer = killname(buf, monst, false);
+
+  time_t date;
+  time(&date);
+  struct tm* lt = localtime(&date);
+
+  move(8, 0);
+
+  char const** dp = rip;
+  while (*dp)
+    addstr(*dp++);
+
+  mvaddstr(17, center(killer), killer);
+  if (monst == 's' || monst == 'h')
+    mvaddcch(16, 32, ' ');
+  else
+    mvaddstr(16, 33, vowelstr(killer));
+
+  mvaddstr(14, center(whoami), whoami);
+  sprintf(buf, "%d Au", purse);
+  move(15, center(buf));
+  addstr(buf);
+  sprintf(buf, "%4d", 1900+lt->tm_year);
+  mvaddstr(18, 26, buf);
+
+  mvprintw(LINES -1, 0, "[Press return to continue]");
+  refresh();
+  wait_for(KEY_ENTER);
+  pack_evaluate();
+  score(purse, pack_contains_amulet() ? 3 : 0, monst);
+  exit(0);
 }
 
 
 void
 total_winner(void)
 {
-    clear();
-    standout();
-    addstr("                                                               \n");
-    addstr("  @   @               @   @           @          @@@  @     @  \n");
-    addstr("  @   @               @@ @@           @           @   @     @  \n");
-    addstr("  @   @  @@@  @   @   @ @ @  @@@   @@@@  @@@      @  @@@    @  \n");
-    addstr("   @@@@ @   @ @   @   @   @     @ @   @ @   @     @   @     @  \n");
-    addstr("      @ @   @ @   @   @   @  @@@@ @   @ @@@@@     @   @     @  \n");
-    addstr("  @   @ @   @ @  @@   @   @ @   @ @   @ @         @   @  @     \n");
-    addstr("   @@@   @@@   @@ @   @   @  @@@@  @@@@  @@@     @@@   @@   @  \n");
-    addstr("                                                               \n");
-    addstr("     Congratulations, you have made it to the light of day!    \n");
-    standend();
-    addstr("\nYou have joined the elite ranks of those who have escaped the\n");
-    addstr("Dungeons of Doom alive.  You journey home and sell all your loot at\n");
-    addstr("a great profit and are admitted to the Fighters' Guild.\n");
-    mvaddstr(LINES - 1, 0, "--Press space to continue--");
-    refresh();
-    wait_for(KEY_SPACE);
-    purse += pack_evaluate();
-    score(purse, 2, ' ');
-    exit(0);
+  clear();
+  standout();
+  addstr("                                                               \n");
+  addstr("  @   @               @   @           @          @@@  @     @  \n");
+  addstr("  @   @               @@ @@           @           @   @     @  \n");
+  addstr("  @   @  @@@  @   @   @ @ @  @@@   @@@@  @@@      @  @@@    @  \n");
+  addstr("   @@@@ @   @ @   @   @   @     @ @   @ @   @     @   @     @  \n");
+  addstr("      @ @   @ @   @   @   @  @@@@ @   @ @@@@@     @   @     @  \n");
+  addstr("  @   @ @   @ @  @@   @   @ @   @ @   @ @         @   @  @     \n");
+  addstr("   @@@   @@@   @@ @   @   @  @@@@  @@@@  @@@     @@@   @@   @  \n");
+  addstr("                                                               \n");
+  addstr("     Congratulations, you have made it to the light of day!    \n");
+  standend();
+  addstr("\nYou have joined the elite ranks of those who have escaped the\n");
+  addstr("Dungeons of Doom alive.  You journey home and sell all your loot at\n");
+  addstr("a great profit and are admitted to the Fighters' Guild.\n");
+  mvaddstr(LINES - 1, 0, "--Press space to continue--");
+  refresh();
+  wait_for(KEY_SPACE);
+  purse += pack_evaluate();
+  score(purse, 2, ' ');
+  exit(0);
 }
 
 
