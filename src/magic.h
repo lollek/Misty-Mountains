37b6496e7d428b0e64843f223b58bc79fb9b910f
Olle Kvarnström <olle+git@iix.se>
2015-06-09 19:09:56 +0200

    move fire_bolt -> magic_bolt

---
+++ src/magic.h
@@ -0,0 +1,9 @@
+#ifndef _ROGUE14_MAGIC_H_
+#define _ROGUE14_MAGIC_H_
+
+#include "coord.h"
+
+/* Fire a bolt in a given direction from a specific starting place */
+void magic_bolt(coord* start, coord* dir, char* name);
+
+#endif /* _ROGUE14_MAGIC_H_ */
