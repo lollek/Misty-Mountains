93831183f788cb4f27c6830b87334b368e5bf12a
Olle Kvarnström <olle+git@iix.se>
2015-06-11 10:56:08 +0200

    static inlines

--- src/monster.h
+++ src/monster.h
@@ -12,76 +12,85 @@ extern int    monster_flytrap_hit;
 bool monsters_save_state(void);
 
 /* Status getters */
-inline bool monster_is_blind(THING const* mon)
+static inline bool monster_is_blind(THING const* mon)
 { return mon->t.t_flags & ISBLIND; }
-inline bool monster_is_cancelled(THING const* mon)
+static inline bool monster_is_cancelled(THING const* mon)
 { return mon->t.t_flags & ISCANC; }
-inline bool monster_is_confused(THING const* mon)
+static inline bool monster_is_confused(THING const* mon)
 { return mon->t.t_flags & ISHUH; }
-inline bool monster_is_confusing(THING const* mon)
+static inline bool monster_is_confusing(THING const* mon)
 { return mon->t.t_flags & CANHUH; }
-inline bool monster_is_found(THING const* mon)
+static inline bool monster_is_found(THING const* mon)
 { return mon->t.t_flags & ISFOUND; }
-inline bool monster_is_hallucinating(THING const* mon)
+static inline bool monster_is_hallucinating(THING const* mon)
 { return mon->t.t_flags & ISHALU; }
-inline bool monster_is_invisible(THING const* mon)
+static inline bool monster_is_invisible(THING const* mon)
 { return mon->t.t_flags & ISINVIS; }
-inline bool monster_is_levitating(THING const* mon)
+static inline bool monster_is_levitating(THING const* mon)
 { return mon->t.t_flags & ISLEVIT; }
-inline bool monster_is_true_seeing(THING const* mon)
+static inline bool monster_is_true_seeing(THING const* mon)
 { return mon->t.t_flags & CANSEE; }
-inline bool monster_is_held(THING const* mon)
+static inline bool monster_is_held(THING const* mon)
 { return mon->t.t_flags & ISHELD; }
-inline bool monster_is_stuck(THING const* mon)
+static inline bool monster_is_stuck(THING const* mon)
 { return mon->t.t_flags & ISSTUCK; }
-inline bool monster_is_chasing(THING const* mon)
+static inline bool monster_is_chasing(THING const* mon)
 { return mon->t.t_flags & ISRUN; }
-inline bool monster_is_mean(THING const* mon)
+static inline bool monster_is_mean(THING const* mon)
 { return mon->t.t_flags & ISMEAN; }
-inline bool monster_is_greedy(THING const* mon)
+static inline bool monster_is_greedy(THING const* mon)
 { return mon->t.t_flags & ISGREED; }
-inline bool monster_is_players_target(THING const* mon)
+static inline bool monster_is_players_target(THING const* mon)
 { return mon->t.t_flags & ISTARGET;}
-inline bool monster_is_slow(THING const* mon)
+static inline bool monster_is_slow(THING const* mon)
 { return mon->t.t_flags & ISSLOW; }
-inline bool monster_is_hasted(THING const* mon)
+static inline bool monster_is_hasted(THING const* mon)
 { return mon->t.t_flags & ISHASTE; }
-inline bool monster_is_flying(THING const* mon)
+static inline bool monster_is_flying(THING const* mon)
 { return mon->t.t_flags & ISFLY; }
 
 /* Status setters */
-inline void monster_set_blind(THING* mon)        { mon->t.t_flags |= ISBLIND; }
-inline void monster_set_cancelled(THING* mon)    { mon->t.t_flags |= ISCANC; }
-inline void monster_set_confused(THING* mon)     { mon->t.t_flags |= ISHUH; }
-inline void monster_set_confusing(THING* mon)    { mon->t.t_flags |= CANHUH; }
-inline void monster_set_found(THING* mon)        { mon->t.t_flags |= ISFOUND; }
-inline void monster_set_hallucinating(THING* mon){ mon->t.t_flags |= ISHALU; }
-inline void monster_set_levitating(THING* mon)   { mon->t.t_flags |= ISLEVIT; }
-inline void monster_set_true_seeing(THING* mon)  { mon->t.t_flags |= CANSEE; }
-inline void monster_become_stuck(THING* mon)     { mon->t.t_flags |= ISSTUCK; }
+static inline void monster_set_blind(THING* mon)
+{ mon->t.t_flags |= ISBLIND; }
+static inline void monster_set_cancelled(THING* mon)
+{ mon->t.t_flags |= ISCANC; }
+static inline void monster_set_confused(THING* mon)
+{ mon->t.t_flags |= ISHUH; }
+static inline void monster_set_confusing(THING* mon)
+{ mon->t.t_flags |= CANHUH; }
+static inline void monster_set_found(THING* mon)
+{ mon->t.t_flags |= ISFOUND; }
+static inline void monster_set_hallucinating(THING* mon)
+{ mon->t.t_flags |= ISHALU; }
+static inline void monster_set_levitating(THING* mon)
+{ mon->t.t_flags |= ISLEVIT; }
+static inline void monster_set_true_seeing(THING* mon)
+{ mon->t.t_flags |= CANSEE; }
+static inline void monster_become_stuck(THING* mon)
+{ mon->t.t_flags |= ISSTUCK; }
 void monster_set_invisible(THING* mon);
 void monster_become_held(THING* monster);
 
 /* Status unsetters */
-inline void monster_remove_blind(THING* mon)
+static inline void monster_remove_blind(THING* mon)
 { mon->t.t_flags &= ~ISBLIND; }
-inline void monster_remove_cancelled(THING* mon)
+static inline void monster_remove_cancelled(THING* mon)
 { mon->t.t_flags &= ~ISCANC; }
-inline void monster_remove_confused(THING* mon)
+static inline void monster_remove_confused(THING* mon)
 { mon->t.t_flags &= ~ISHUH; }
-inline void monster_remove_confusing(THING* mon)
+static inline void monster_remove_confusing(THING* mon)
 { mon->t.t_flags &= ~CANHUH; }
-inline void monster_remove_found(THING* mon)
+static inline void monster_remove_found(THING* mon)
 { mon->t.t_flags &= ~ISFOUND; }
-inline void monster_remove_hallucinating(THING* mon)
+static inline void monster_remove_hallucinating(THING* mon)
 { mon->t.t_flags &= ~ISHALU; }
-inline void monster_remove_invisible(THING* mon)
+static inline void monster_remove_invisible(THING* mon)
 { mon->t.t_flags &= ~ISINVIS; }
-inline void monster_remove_levitating(THING* mon)
+static inline void monster_remove_levitating(THING* mon)
 { mon->t.t_flags &= ~ISLEVIT; }
-inline void monster_remove_true_seeing(THING* mon)
+static inline void monster_remove_true_seeing(THING* mon)
 { mon->t.t_flags &= ~CANSEE; }
-inline void monster_remove_held(THING* mon)
+static inline void monster_remove_held(THING* mon)
 { mon->t.t_flags &= ~ISHELD; }
 
 /* Pick a monster to show up.  The lower the level, the meaner the monster. */
