417988708e3362e1220fc24de6eed038f7ca0cff
Olle Kvarnström <olle+git@iix.se>
2016-03-01 14:55:48 +0100

    Syntax fix

--- src/daemons.h
+++ src/daemons.h
@@ -24,36 +24,36 @@ enum daemon_function {
 };
 
 struct Fuse {
-    int type;
-    Daemons::daemon_function func;
-    int time;
+  int type;
+  Daemons::daemon_function func;
+  int time;
 };
 
 struct Daemon {
-    int type;
-    Daemons::daemon_function func;
+  int type;
+  Daemons::daemon_function func;
 };
 
-/* API */
+// API
 void daemon_run_before();
 void daemon_run_after();
 
-/* Daemons */
+// Daemons
 void daemon_start(daemon_function func, int type);
 void daemon_kill(daemon_function func);
 
-/* Fuses */
+// Fuses
 void daemon_start_fuse(daemon_function func, int time, int type);
 void daemon_extinguish_fuse(daemon_function func);
 void daemon_lengthen_fuse(daemon_function func, int xtime);
 
-/* Daemon actions */
+// Daemon actions
 void daemon_doctor();
 void daemon_change_visuals();
 void daemon_runners_move();
 void daemon_ring_abilities();
 
-/* Daemon action affectors */
+// Daemon action affectors
 void daemon_reset_doctor();
 
 }
